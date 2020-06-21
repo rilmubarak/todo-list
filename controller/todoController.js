4903e045f298f7d39491f68b6a65c371e5e7b4aa
mubaraqhere <mubaraqhere@gmail.com>
2020-06-21 22:33:12 +0700

    add all todo standars

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const View = require('../view/viewTodo')
 const Model = require('../models/todo')
 
@@ -6,9 +8,34 @@ class Controller {
         View.help()
     }
 
-    static list() {
+    static allList() {
         let res = Model.list()
+        View.printList(res)
+    }
+
+    static addData(priority, input) {
+        let res = Model.add(priority, input.join(' '))
         View.print(res)
+    }
+
+    static search(input) {
+        let res = Model.find(+input)
+        View.find(res)
+    }
+
+    static deleteTodo(input) {
+        let res = Model.delete(+input)
+        View.delete(res)
+    }
+
+    static completeTodo(input) {
+        let res = Model.complete(+input)
+        View.complete(res)
+    }
+
+    static uncompleteTodo(input) {
+        let res = Model.uncomplete(+input)
+        View.uncomplete(res)
     }
 }
 
