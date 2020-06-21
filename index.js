f37a4e4fa694913a8ea7aa20e665548605e5e198
mubaraqhere <mubaraqhere@gmail.com>
2020-06-21 22:32:16 +0700

    add some sintax argv

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const Controller = require('./controller/todoController')
+const Controller = require('./controller/todoController');
+const { argv } = require('process');
 const Argv = process.argv.slice(2)
 const command = Argv[0]
 
+const data = Argv.slice(1)
 
 switch (command) {
     case undefined                  : Controller.help(); break;
-    case 'help'                     : Controller.help()
-    case 'list'                     : Controller.list()
-    // case 'add'                      : Controller.add()
-    // case 'findById'                 : Controller.find()
-    // case 'delete'                   : Controller.delete()
-    // case 'complete'                 : Controller.complete()
-    // case 'uncomplete'               : Controller.uncomplete()
+    case 'help'                     : Controller.help(); break
+    case 'list'                     : Controller.allList(); break;
+    case 'add'                      : Controller.addData(data[0], data.slice(1)); break;
+    case 'findById'                 : Controller.search(data[0]); break;
+    case 'delete'                   : Controller.deleteTodo(data[0]); break;
+    case 'complete'                 : Controller.completeTodo(data[0]); break;
+    case 'uncomplete'               : Controller.uncompleteTodo(data[0]); break;
 }
